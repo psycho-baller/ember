20e14dcbf941cf096c8935c302af696b593a70e6
Rami Maalouf <rami.rami@ucalgary.ca>
2025-08-26 19:13:00 -0600

    better spinner

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -4,10 +4,10 @@ export default function Spinner() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="h-8 w-8 animate-spin rounded-full border-4 border-foreground border-t-transparent"></div>
-      <div className="relative flex justify-center items-center h-14 w-14 mx-auto my-6">
+      {/* <div className="relative flex justify-center items-center h-14 w-14 mx-auto my-6">
           <div className="absolute inset-0 border-2 border-primary/20 rounded-full"></div>
           <div className="absolute inset-0 border-t-2 border-r-2 border-primary rounded-full animate-spin"></div>
-        </div>
+        </div> */}
     </div>
   );
 }
