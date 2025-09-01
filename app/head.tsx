8641689a00f0680c11445d14aca05480b24eef56
Rami Maalouf <rami.rami@ucalgary.ca>
2025-09-01 11:45:58 -0600

    added head tags for analytics

---
+++ app/head.tsx
@@ -0,0 +1,7 @@
+export default function Head() {
+  return (
+    <>
+    <script defer src="https://cloud.umami.is/script.js" data-website-id="81fcd805-b14a-4d2c-8186-332a9e0c7064"></script>
+  </>
+  );
+  }
