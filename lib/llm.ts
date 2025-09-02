d7e1373045566ff8f5a96caa6e7cb0ed1d51e564
Rami Maalouf <rami.rami@ucalgary.ca>
2025-09-02 09:29:50 -0600

    we now have a json llm response

--- lib/llm.ts
+++ lib/llm.ts
@@ -1,7 +1,8 @@
 import { OpenAI } from 'openai'
 import { openai } from "@ai-sdk/openai"
-import { generateText, LanguageModel, ModelMessage } from "ai"
+import { generateObject, generateText, LanguageModel, ModelMessage } from "ai"
 import { DEFAULT_SYSTEM_PROMPT } from './prompts'
+import { z } from 'zod';
 export async function callBasicLlm(prompt: string): Promise<string> {
   const apiKey = process.env.OPENAI_API_KEY
 
@@ -29,4 +30,21 @@ export async function callLlm(messages: ModelMessage[] = [], system: string = DE
     messages,
   })
   return text
+}
+
+/**
+ * Make the AI return a json object
+ */
+export async function callLlmJson<T>(prompt: string, schema: z.ZodType<T>, model: LanguageModel = openai('gpt-4o-mini')): Promise<T> {
+  const apiKey = process.env.OPENAI_API_KEY
+
+  if (!apiKey) {
+    throw new Error('OPENAI_API_KEY environment variable is not set')
+  }
+  const { object } = await generateObject({
+    model,
+    prompt,
+    schema,
+  })
+  return object as T
 }
