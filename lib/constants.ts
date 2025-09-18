20baf28d899bb83f1154187b5ce5e7cecafac1da
Rami Maalouf <rami.rami@ucalgary.ca>
2025-09-17 22:12:28 -0600

    big fix for ucalgary vs uw

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
 export const env = {
-  LOCATION_ID: process.env.NEXT_PUBLIC_LOCATION_ID || "uofc",
+  LOCATION_ID: process.env.NEXT_PUBLIC_LOCATION_ID || process.env.LOCATION_ID || process.env.TWILIO_PHONE_NUMBER === "+18076977967" ? "uw" : "uofc",
+
 };
